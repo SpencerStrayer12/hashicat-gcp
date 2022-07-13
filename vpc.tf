8d8faad567d69155ccc36b58b2bcb7dfc1672fbd
Spencer Strayer <spencerstrayer12@gmail.com>
2022-07-13 21:52:28 +0000

    added vpc module

---
+++ vpc.tf
@@ -0,0 +1,13 @@
+module "network" {
+  source  = "app.terraform.io/chip-cert-org/network/google"
+  version = "3.4.0"
+  network_name = "CHIPcert"
+  project_id = var.project_id
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
